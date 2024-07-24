87d2dfab5e66489f4bd3f876d19a23d0cefbd8f7
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-07-24 22:02:36 +0530

    signup routes

--- primary--backend/src/router/user.ts
+++ primary--backend/src/router/user.ts
@@ -1,11 +1,46 @@
 
 import { Router } from "express";
 import { authMiddleware } from "../middleware";
-
+import { signinSchema, signupSchema } from "../types/types";
+import { prismaClient } from "../db";
 const router = Router();
 
-router.post("/signup", (req, res) => {
-    console.log("signup handler");
+router.post("/signup", async (req, res) => {
+    const body = req.body.username;
+    const parsedData = signupSchema.safeParse(body);
+
+    if (!parsedData.success) {
+        return res.status(411).json({
+            message:"Incorrect inputs"
+        })
+    }
+
+    const userExists = await prismaClient.user.findFirst({
+        where: {
+            email:parsedData.data.username
+        }
+    })
+
+    if (userExists) {
+        return res.status(403).json({
+            message:"User already exists"
+        })
+    }
+
+    await prismaClient.user.create({
+        data: {
+            email: parsedData.data.username,
+            password:parsedData.data.password,
+            name:parsedData.data.name
+        }
+    })
+
+    // send email logic
+
+    return res.json({
+        message:"Please verify account"
+    })
+
 })
 
 router.post("/signin", (req, res) => {
