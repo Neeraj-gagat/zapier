9f0bd61c2b13ec1eeff52abff28374873ff65571
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-07-29 23:50:27 +0530

    adding dashBoard

--- frontend/components/buttons/LinkButton.tsx
+++ frontend/components/buttons/LinkButton.tsx
@@ -3,7 +3,7 @@
 import { ReactNode } from "react"
 
 export const LinkButton = ({children, onClick}:{ children: ReactNode, onClick: () => void }) => {
-    return <div className="px-2 py-2 rounded-sm font-light text-sm cursor-pointer hover:bg-slate-100" onClick={onClick}>
+    return <div className=" flex justify-center px-2 py-2 rounded font-light text-sm cursor-pointer hover:bg-slate-100" onClick={onClick}>
         {children}
     </div>
 }
--- frontend/components/buttons/SecondaryButton.tsx
+++ frontend/components/buttons/SecondaryButton.tsx
@@ -5,7 +5,7 @@ export const SecondaryButton = ({children, onClick, size = "small"}: {
     onClick: () => void,
     size?: "big"|"small"}) => {
     return <div onClick={onClick} className={`${size === "small" ? "text-sm" : "text-xl"}
-    ${size === "small" ? "px-7 py-2" : "px-10 py-3"} hover:shadow-xl cursor-pointer border border-black rounded-full font-semibold text-black`}>
+    ${size === "small" ? "px-7 py-2" : "px-10 py-3"} hover:shadow-xl  cursor-pointer border border-black rounded-full font-semibold text-black`}>
         {children}
     </div>
 }
