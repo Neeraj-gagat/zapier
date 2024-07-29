195f82bd3b401ced80b470f176c7e137fc28db9e
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-07-29 21:52:03 +0530

    adding Darkbutton

---
+++ frontend/components/buttons/DarkButton.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+export const DarkButton = ({children, onClick, size = "small"}: {
+    children:ReactNode,
+    onClick: () => void,
+    size?: "big"|"small"}) => {
+    return <button onClick={onClick} className={`${size === "small" ? "text-sm" : "text-xl"}
+    ${size === "small" ? "px-7 py-2" : "px-10 py-3"} font-medium hover:shadow-xl  cursor-pointer bg-blue-900 rounded text-center text-white`}>
+        {children}
+    </button>
+}
