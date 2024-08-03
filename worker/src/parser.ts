02d3eb6454365cd212354251bc6c92cbd3403aff
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-08-03 20:46:26 +0530

    adding string parser

---
+++ worker/src/parser.ts
@@ -0,0 +1,39 @@
+
+export function parse(text: string, value:any, startDelimeter = "{", endDelemeter = "}"){
+    let startIndex  = 0;
+    let endIndex  = 1;
+
+    let  finalString = "";
+    while (endIndex != text.length) {
+        if (text[startIndex] === startDelimeter) {
+            let endPoint = startIndex + 2;
+            while (text[endPoint] !== endDelemeter) {
+                endPoint++;
+            }
+            // 
+            let stringHoldingValue = text.slice(startIndex + 1, endPoint);
+            const keys = stringHoldingValue.split(".");
+            let localValue = {
+                ...value
+            }
+            for (let i = 0; i < keys.length; i++) {
+                if (typeof localValue === "string") {
+                    localValue = JSON.parse(localValue);
+                }
+                localValue = localValue[keys[i]];
+            }
+            finalString += localValue; 
+            startIndex = endPoint + 1;
+            endIndex = endPoint + 2;
+        }else{
+            finalString += text[startIndex];
+            startIndex++;
+            endIndex++;
+        }
+    }
+    if (text[startIndex]) {
+        finalString += text[startIndex]
+    }
+
+    return finalString;
+}
